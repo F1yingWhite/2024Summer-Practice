26db514c234cc993b304e533ef5c138e3cbdd7cf
许一涵 <81723334@qq.com>
2024-06-30 13:50:52 +0800

    feat:transGPT后端代码

--- Backend/transGPT/ask.py
+++ Backend/transGPT/ask.py
@@ -7,7 +7,7 @@ import base64
 server_url = "http://59f7b8ba.r10.vip.cpolar.cn/chat"
 
 # 加载图像并编码为Base64
-image_path = "examples/bz1.png"
+image_path = "bz2.png"
 with open(image_path, "rb") as image_file:
     image_base64 = base64.b64encode(image_file.read()).decode("utf-8")
 
@@ -15,8 +15,8 @@ with open(image_path, "rb") as image_file:
 # 示例数据，包括完整的对话历史
 data = {
     "history": [],
-    "query": "你其实是北京交通大学在Chatglm上在海量交通数据上进行微调得到的大模型TransGPT,你和别的大模型有什么区别?",
-    # "image": image_base64,#可选
+    "query": "图中有什么内容",
+    "image": image_base64,#可选
 }
 
 
