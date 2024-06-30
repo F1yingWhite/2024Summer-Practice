fc605502ff0ff6d6e72cc11cf2e67188910e5fed
ricetea <1610473094@qq.com>
2024-06-30 11:26:37 +0800

    前后端仓库分离

--- Front/src/lib/axios/axios.ts
+++ Front/src/lib/axios/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 // const host = 'http://192.168.43.128:3308'
 // const host = 'http://123.60.53.131:3308'
 // const host = 'http://120.46.129.79:3308'
-const host = 'http://124.70.109.243:3308'
+const host = 'http://localhost:3308'
 
 export function get(url: string): Promise<any> {
   return new Promise((resolve, reject) => {
